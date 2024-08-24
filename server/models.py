638addbfe80d2510b41600e7c903f5576b4bb957
mercynjambi <njambim695@gmail.com>
2024-08-24 10:54:10 +0300

    passed the tests

--- server/models.py
+++ server/models.py
@@ -5,5 +5,11 @@ db = SQLAlchemy()
 
 class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
-
+    
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)  # Name is required
+    image = db.Column(db.String)  # Allow image to be null
+    price = db.Column(db.Float)   # Allow price to be null
+
+    def __repr__(self):
+        return f'<Plant {self.name}>'
